e8a15088aa55316726dc0fcef3dfed76b8ea0606
Andrew Brinker <me@andrewbrinker.com>
2014-04-26 22:34:41 -0700

    Okay, the logic should work better now

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,12 @@ $("#main-nav-trigger").click(function (e) {
  * NOTE: Smaller screen devices will still download the image currently. This is
  * simply a test to show that the conditional loading logic works.
  */
-window.addEventListener("resize", function() {
+var header_bg_toggle = function () {
   if (Modernizr.mq('only screen and (min-width: 700px)')) {
     $("body").addClass("background-image");
   } else {
     $("body").removeClass("background-image");
   }
-}, true);
+}
+header_bg_toggle();
+window.addEventListener("resize", header_bg_toggle, true);
