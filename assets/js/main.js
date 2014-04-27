d8499961df035df088a9b6820f6acc2797e1ffef
Andrew Brinker <me@andrewbrinker.com>
2014-04-27 10:54:55 -0700

    Fixing up some responsive stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,12 +15,24 @@ $("#main-nav-trigger").click(function (e) {
  * NOTE: Smaller screen devices will still download the image currently. This is
  * simply a test to show that the conditional loading logic works.
  */
-var header_bg_toggle = function () {
+function header_bg_toggle() {
   if (Modernizr.mq('only screen and (min-width: 700px)')) {
     $("body").addClass("background-image");
   } else {
     $("body").removeClass("background-image");
   }
 }
+
+function set_campus_link() {
+    var campus = document.getElementById('campus-trigger');
+    if (Modernizr.mq('only screen and (min-width: 700px')) {
+        campus.href = "#";
+    } else {
+        campus.href = "http://csusb.edu";
+    }
+}
+
 header_bg_toggle();
+set_campus_link();
 window.addEventListener("resize", header_bg_toggle, true);
+window.addEventListener("resize", set_campus_link, true);
