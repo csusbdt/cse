41bcda9193fbcd1b7e7acb8d486710dfe000c4c4
Andrew Brinker <me@andrewbrinker.com>
2014-04-26 22:22:59 -0700

    Trying to get conditional image loading done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
+/**
+ * Navigation toggle. Toggles the js-visible class on the navigation, and
+ * turns off the default click event for the trigger.
+ *
+ * @param  {event} e -> The default event triggered on click
+ */
 $("#main-nav-trigger").click(function (e) {
   $("#main-nav").toggleClass("js-visible");
   e.preventDefault();
 });
+
+/**
+ * Conditional loading of resources.
+ */
+window.addEventListener("resize", function() {
+  if (Modernizr.mq('only screen and (min-width: 700px)')) {
+    $("body").addClass("background-image");
+  } else {
+    $("body").removeClass("background-image");
+  }
+}, true);
