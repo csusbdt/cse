2fa3055d5b68b32ae8d46b11e489c2525a2183e6
Andrew Brinker <me@andrewbrinker.com>
2014-04-27 11:29:24 -0700

    Organized the javascript a bit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,57 @@
-/**
- * Navigation toggle. Toggles the js-visible class on the navigation, and
- * turns off the default click event for the trigger.
- *
- * @param  {event} e -> The default event triggered on click
- */
-$("#main-nav-trigger").click(function (e) {
-  $("#main-nav").toggleClass("js-visible");
-  e.preventDefault();
-});
+(function () {
+    /**
+     * Navigation toggle for small screens.
+     */
+    var NavToggle = {
+        // Separating the things I'm referencing from when I'm referencing them.
+        settings: {
+            navTrigger: $("#main-nav-trigger"),
+            nav: $("#main-nav")
+        },
 
-/**
- * Conditional loading of resources.
- *
- * NOTE: Smaller screen devices will still download the image currently. This is
- * simply a test to show that the conditional loading logic works.
- */
-function header_bg_toggle() {
-  if (Modernizr.mq('only screen and (min-width: 700px)')) {
-    $("body").addClass("background-image");
-  } else {
-    $("body").removeClass("background-image");
-  }
-}
+        // The initialization function.
+        init: function() {
+            self = this.settings;
+            self.navTrigger.click(function (e) {
+                self.nav.toggleClass("js-visible");
+                e.preventDefault();
+            });
+        }
+    };
 
-function set_campus_link() {
-    var campus = document.getElementById('campus-trigger');
-    if (Modernizr.mq('only screen and (min-width: 700px')) {
-        campus.href = "#";
-    } else {
-        campus.href = "http://csusb.edu";
-    }
-}
+    /**
+     * Handle responsive changes
+     */
+    var Responsive = {
+        // Header background toggle function
+        header_bg_toggle: function () {
+            if (Modernizr.mq('only screen and (min-width: 700px)')) {
+                $("body").addClass("background-image");
+            } else {
+                $("body").removeClass("background-image");
+            }
+        },
 
-header_bg_toggle();
-set_campus_link();
-window.addEventListener("resize", header_bg_toggle, true);
-window.addEventListener("resize", set_campus_link, true);
+        // Set campus toggle link
+        set_campus_link: function () {
+            var campus = document.getElementById('campus-trigger');
+            if (Modernizr.mq('only screen and (min-width: 700px')) {
+                campus.href = "#";
+            } else {
+                campus.href = "http://csusb.edu";
+            }
+        },
+
+        // Initialization function
+        init: function () {
+          this.header_bg_toggle();
+          this.set_campus_link();
+          window.addEventListener("resize", this.header_bg_toggle, true);
+          window.addEventListener("resize", this.set_campus_link, true);
+        }
+    };
+
+    // Run everything.
+    NavToggle.init();
+    Responsive.init();
+})();
