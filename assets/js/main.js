832fa8c3a1d49abe0057d8e318ba8f39053c1179
Andrew Brinker <me@andrewbrinker.com>
2014-04-28 10:32:30 -0700

    Progress on desktop navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,36 @@
-(function () {
-    /**
-     * Navigation toggle for small screens.
-     */
-    var NavToggle = {
-        // Separating the things I'm referencing from when I'm referencing them.
-        settings: {
-            nav: $("#main-nav"),
-            navTrigger: $("#main-nav-trigger"),
-            navTriggerIcon: $("#main-nav-trigger span")
-        },
+var NavToggle = {
+  settings: {
+    nav: $("#main-nav"),
+    navTrigger: $("#main-nav-trigger"),
+    navTriggerIcon: $("#main-nav-trigger span")
+  },
+  init: function () {
+    var self = this.settings;
+    self.navTrigger.click(function (e) {
+      self.nav.toggleClass("js-visible");
+      self.navTriggerIcon.toggleClass("icon-arrow-down");
+      self.navTriggerIcon.toggleClass("icon-arrow-up");
+      e.preventDefault();
+    });
+  }
+};
 
-        // The initialization function.
-        init: function () {
-            self = this.settings;
-            self.navTrigger.click(function (e) {
-                self.nav.toggleClass("js-visible");
-                self.navTriggerIcon.toggleClass("icon-arrow-down");
-                self.navTriggerIcon.toggleClass("icon-arrow-up");
-                e.preventDefault();
-            });
-        }
-    };
+var Responsive = {
+  respond: function () {
+    if (Modernizr.mq("only screen and (min-width: 700px)")) {
+      $("body").addClass("background-image");
+      $("#campus-trigger").href = "#";
+    } else {
+      $("body").removeClass("background-image");
+      $("#campus-trigger").href = "http://csusb.edu";
+    }
+  },
+  init: function () {
+    this.respond();
+    window.addEventListener("resize", this.respond, true);
+  }
+};
 
-    /**
-     * Handle responsive changes
-     */
-    var Responsive = {
-        // Header background toggle function
-        header_bg_toggle: function () {
-            if (Modernizr.mq('only screen and (min-width: 700px)')) {
-                $("body").addClass("background-image");
-            } else {
-                $("body").removeClass("background-image");
-            }
-        },
+NavToggle.init();
+Responsive.init();
 
-        // Set campus toggle link
-        set_campus_link: function () {
-            var campus = document.getElementById('campus-trigger');
-            if (Modernizr.mq('only screen and (min-width: 700px')) {
-                campus.href = "#";
-            } else {
-                campus.href = "http://csusb.edu";
-            }
-        },
-
-        // Initialization function
-        init: function () {
-          this.header_bg_toggle();
-          this.set_campus_link();
-          window.addEventListener("resize", this.header_bg_toggle, true);
-          window.addEventListener("resize", this.set_campus_link, true);
-        }
-    };
-
-    // Run everything.
-    NavToggle.init();
-    Responsive.init();
-})();
